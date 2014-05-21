d7e1ef05291708682436f4ccd6254c350fb30406
Nathan Sutcliffe <nscloud9@nathansutcliffe.net>
2014-05-20 23:23:28 -0400

    improvements for Cloud9

--- helloworld.js
+++ helloworld.js
@@ -2,11 +2,14 @@ var serverPort = process.env.PORT; // Try to use port number from Cloud 9 config
 if (serverPort === undefined)
   serverPort = 1337;
 
+var isCloud9 = (process.env.C9_USER !== undefined);
+
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Stonehenge\n');
 }).listen(serverPort);
 
-console.log('Server running at http://127.0.0.1:' + serverPort + '/');
-console.log(process.env);
+if (!isCloud9)
+  console.log('Server running at http://127.0.0.1:' + serverPort + '/');
+//console.log(process.env);
