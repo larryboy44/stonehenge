d9423df9f11d0a6b089d9501fe74994bb46a750b
Nathan Sutcliffe <nathan@sutcliffe.ca>
2014-05-20 19:13:51 -0700

    Adding Eclipse project file

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,12 @@
+var serverPort = process.env.PORT; // Try to use port number from Cloud 9 config
+if (serverPort === undefined)
+  serverPort = 1337;
+
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Stonehenge\n');
-}).listen(process.env.PORT); // Not sure if process.env.PORT is specific to Cloud 9
+}).listen(serverPort);
 
+console.log('Server running at http://127.0.0.1:' + serverPort + '/');
+console.log(process.env);
